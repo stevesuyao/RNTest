ad5e0700a028b792977b4c3773a1617788a6a86b
Steve <stevesuyao@gmail.com>
2018-11-09 00:44:58 -0500

    add navigation apis

---
+++ app/services/navigation.js
@@ -0,0 +1,67 @@
+import { Navigation } from 'react-native-navigation';
+import screenIDs from '../constants/screenIDs';
+
+export const goHome = () => Navigation.setRoot({
+  root: {
+    sideMenu: {
+      left: {
+        component: {
+          name: screenIDs.PROFILE,
+          passProps: {
+            text: 'This is a left side menu screen',
+          },
+        },
+      },
+      center: {
+        stack: {
+          id: 'main_stack',
+          children: [
+            {
+              component: {
+                name: screenIDs.MAIN,
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+});
+
+export const goAuth = () => Navigation.setRoot({
+  root: {
+    stack: {
+      id: 'Auth',
+      children: [
+        {
+          component: {
+            name: screenIDs.LOGIN,
+          },
+        },
+      ],
+    },
+  },
+});
+
+export const popOut = id => Navigation.pop(id);
+
+export const pushIn = (id, name) => Navigation.push(id, { component: { name } });
+
+export const startInitalScreen = () => {
+  Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setDefaultOptions({ // global screen's style. Must set before setRoot();
+      layout: {
+      // backgroundColor: 'white',
+        orientation: ['portrait'], // An array of supported orientations
+      },
+    });
+
+    Navigation.setRoot({
+      root: {
+        component: {
+          name: screenIDs.INIT,
+        },
+      },
+    });
+  });
+};
