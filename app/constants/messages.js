2dce323a3302da13b7a2ab6e20d16306e825d0ba
Steve <stevesuyao@gmail.com>
2018-11-12 17:57:26 -0500

    update constants

---
+++ app/constants/messages.js
@@ -0,0 +1,19 @@
+// types
+const ERROR = 'ERROR';
+const HINT = 'HINT';
+const WARN = 'WARN';
+
+export const messageTypes = {
+  ERROR,
+  HINT,
+  WARN,
+};
+
+// contents
+
+const PURGE_ERROR = 'Failed to clear local data.';
+
+
+export const messageContents = {
+  PURGE_ERROR,
+};
