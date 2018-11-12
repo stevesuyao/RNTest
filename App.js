6fcb8f9164130a7103529ad5c83597a56d283e55
Steve <stevesuyao@gmail.com>
2018-11-12 17:58:00 -0500

    update app class and entry point

--- App.js
+++ App.js
@@ -4,42 +4,144 @@
  * @flow
  */
 
+import _ from 'lodash';
 import type { Persistor } from 'redux-persist/es/types';
-import type { Provider } from 'react-redux';
-import type { Store } from 'redux';
-import { startInitalScreen } from './app/services/navigation';
-import type { ScreenConfig } from './app/flowTypes';
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import type { Store as ReduxStore, Reducer as ReduxReducer } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import createSenesitveStorage from 'redux-persist-sensitive-storage';
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import * as NavigationApis from './app/services/navigation';
+import type { ScreenConfig, SenesitveStorageConfig, PersistConfig } from './app/flowTypes';
 import { registerScreens } from './app/screens';
+import createRootReducer from './app/reducers';
+import configureStore from './app/store';
 
 
 export default class App {
-  constructor(screens: Array<ScreenConfig>, store: Store, persistor: Persistor, provider: Provider) {
-    this.screens = screens;
-    this.store = store;
-    this.persistor = persistor;
-    this.provider = provider;
+  constructor(screenConfigs: Array<ScreenConfig>) {
+    this.screens = screenConfigs;
+    this.storage = null;
+    this.rootReducer = null;
+    this.sensitiveReducer = null;
+    this.store = null;
+    this.sensitiveStorage = null;
+    this.persistor = null;
+    this._nativeIcons = {};
+    this._loadIconImages = () => null;
   }
 
   screens: Array<ScreenConfig>
 
-  store: Store
+  storage: any
 
-  persistor: Persistor
+  store: ReduxStore
 
-  provider: Provider
+  persistor: null | Persistor
+
+  sensitiveStorage: any
+
+  rootReducer: ReduxReducer
+
+  sensitiveReducer: ReduxReducer
+
+  _nativeIcons: Object
+
+  _loadIconImages: Function
+
+  setLocalStorage = (storage: any) => {
+    this.storage = storage;
+  };
+
+  setSensitiveStorage = (senesitveStorageConfig:SenesitveStorageConfig) => {
+    if (!senesitveStorageConfig) throw new Error('sensitive storage config not found.');
+    this.sensitiveStorage = createSenesitveStorage(senesitveStorageConfig);
+  }
+
+  _createPersistConfig = (persistConfig: PersistConfig) => ({
+    ...persistConfig,
+    storage: this.storage,
+  });
+
+ _createSensitivePersistConifg = (sensitivePersistConfig: PersistConfig) => ({
+   ...sensitivePersistConfig,
+   storage: this.sensitiveStorage,
+ })
+
+  setSensitiveReducer = (sensitiveReducer: ReduxReducer, sensitivePersistConfig: PersistConfig) => {
+    if (!sensitiveReducer) throw new Error('sensitive reducer should not be null.');
+    if (!this.sensitiveStorage) throw new Error('sensitive storage have not been created.');
+    this.sensitiveReducer = persistReducer(this._createSensitivePersistConifg(sensitivePersistConfig), sensitiveReducer);
+  }
+
+  setRootReducer = (defaultReducer: ReduxReducer, persistConfig: PersistConfig = null) => {
+    const rootReducer = createRootReducer(this.screens, defaultReducer);
+    if (this.storage && persistConfig) this.rootReducer = persistReducer(this._createPersistConfig(persistConfig), rootReducer);
+    else this.rootReducer = rootReducer;
+  }
+
+  setStore = (...middleware:any) => {
+    if (!this.rootReducer) throw new Error('root reducer has not been set.');
+    if (this.sensitiveReducer) {
+      const reducer = combineReducers({
+        main: this.rootReducer,
+        sensitive: this.sensitiveReducer,
+      });
+      this.store = configureStore(reducer, ...middleware);
+    } else this.store = configureStore(this.rootReducer, ...middleware);
+  }
+
+  _setPersistStore = () => {
+    if (!this.storage) return null;
+    return new Promise((resolve) => {
+      this.persistor = persistStore(this.store, undefined, () => {
+        console.log('persiste store resolved.');
+        resolve(true);
+      });
+    });
+  }
 
   /*
    * @brief Register all screens components to navigator.
    */
-  _registerScreens() {
+  _registerScreens = () => {
+    if (!this.screens) throw new Error('pls config least one screen.');
     if (!this.store) throw new Error('redux store has not been configured.');
-    registerScreens(this.screens, this.store, this.provider);
+    registerScreens(this.screens, this.store, Provider);
   }
 
+  _setNativeIcons = (obj, icon, size) => new Promise((resolve) => {
+    icon.getImageSource(obj.value, size).then((value) => {
+      this._nativeIcons[obj.key] = value;
+      resolve(true);
+    });
+  });
+  // this._nativeIcons[obj.key] = icon.getImageSource(obj.value, size);
 
-  startApp() {
+  setIconImages = (config: Object, icoMoonSeletions: Object) => {
+    if (!config || !icoMoonSeletions) throw new Error('please add config or an icoMoon json file first.');
+    const iconObjs = [];
+    _.forEach(config, (value, key) => iconObjs.push({ key, value }));
+    if (iconObjs.length === 0) throw new Error('config file should contain least one key-value pair.');
+
+    const icon = createIconSetFromIcoMoon(icoMoonSeletions);
+
+    this._loadIconImages = () => _.map(iconObjs, obj => this._setNativeIcons(obj, icon, 30)); // promise array
+  }
+
+  startApp = () => {
+    const state = this.store.getState();
+    if (state.sensitive.user) NavigationApis.goHome();
+    // if (this.persistor) startInitalScreen({ localPurge: this.persistor.purge });
+    else NavigationApis.goAuth();
+  }
+
+  bootStrap = async () => {
     this._registerScreens();
-    // Start app only if all icons are loaded.
-    startInitalScreen();
+
+    await Promise.all([this._setPersistStore(), ...this._loadIconImages()]);
+
+    this.startApp();
   }
 }
--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 /*
  * Entry point of the react native app.
  * @format
+ *
+ * @flow
  */
-import { Provider } from 'react-redux';
-import { screenConfigs } from './app/screens';
-import { store, persistor } from './app/store';
-import App from './App';
 
-const app = new App(screenConfigs, store, persistor, Provider);
-app.startApp();
+import { AsyncStorage } from 'react-native';
+import createSagaMiddleware from 'redux-saga';
+import { screenConfigs } from './app/screens';
+import App from './App';
+import rootSaga from './app/sagas';
+import defaultReducer from './app/reducers/defaultReducer';
+import {
+  persistConfig, sensitivePersistConfig, senesitveStorageConfig, iconImagesConfig,
+} from './app/config';
+import sensitiveReducer from './app/reducers/sensitiveReducer';
+import icoMoonSeletions from './app/assets/vectors/selection.json';
+
+const app = new App(screenConfigs);
+
+// set app localStorage
+app.setLocalStorage(AsyncStorage);
+
+// set app sensitive info storage
+app.setSensitiveStorage(senesitveStorageConfig);
+
+// create sensitive reducer
+app.setSensitiveReducer(sensitiveReducer, sensitivePersistConfig);
+
+// create root reducer.
+app.setRootReducer(defaultReducer, persistConfig);
+
+// create redux middleware.
+const sagaMiddleware = createSagaMiddleware();
+
+// configure redux store and apply middlewares.
+app.setStore(sagaMiddleware);
+
+// run saga middleware after apply it to store.
+sagaMiddleware.run(rootSaga);
+
+// set native button images.
+app.setIconImages(iconImagesConfig, icoMoonSeletions);
+
+// start app.
+app.bootStrap();
