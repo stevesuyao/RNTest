9507eabf4070697c56c973abe6066ce7d0bcf1d1
Steve <stevesuyao@gmail.com>
2018-11-12 17:57:04 -0500

    update flow types

--- app/flowTypes.js
+++ app/flowTypes.js
@@ -14,3 +14,34 @@ export type ScreenConfig = {
   id: string,
   reducer?: null | Function,
 }
+
+// Meesage data type
+export type Message = {
+  content: string,
+  type: string,
+};
+
+export type MiddlewareObject = {
+  middleware: any,
+  run: Function,
+};
+
+export type ActionType = {
+  REQUEST: string,
+  SUCCESS: string,
+  FAILURE: string,
+  CANCELLED: string,
+  KEY: string,
+  hasPayload: boolean,
+};
+
+export type SenesitveStorageConfig = {
+  keychainService: string,
+  sharedPreferencesName: string,
+};
+
+export type PersistConfig = {
+  key: string,
+  blacklist?: Array<string>,
+  whitelist?: Array<string>,
+} | null;
